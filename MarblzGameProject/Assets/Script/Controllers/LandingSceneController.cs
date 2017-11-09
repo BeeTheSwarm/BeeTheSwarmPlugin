7282ec7964cf89c61a647b0533df1daf53c80c1e
Andrew <gunship07@gmail.com>
2017-11-09 18:02:41 +0200

    add Landing scene.

---
+++ MarblzGameProject/Assets/Script/Controllers/LandingSceneController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LandingSceneController : SA_Singleton<LandingSceneController>  {
+
+	public static bool isNotFirstTimeLaunch = false;
+
+	[SerializeField] List<GameObject> _prefabsToLoad;
+
+	void Awake(){
+	
+		DontDestroyOnLoad (gameObject);
+		Application.targetFrameRate = 30;
+
+	}
+
+
+	void Start(){
+
+		GoToLevel ();
+		Debug.Log ("Connection started");
+
+		LoadPrefabs ();
+	}
+
+	private void GoToLevel(){
+
+		Time.timeScale = 1f;
+		StartCoroutine (LoadLevelAfterDelay ("Level", 2.5f));
+
+	}
+
+	private void LoadPrefabs(){
+
+		StartCoroutine (LoadPrefabsCoroutine ());
+	}
+
+	IEnumerator LoadLevelAfterDelay(string levelname, float delay){
+	
+		yield return new WaitForSeconds (delay);
+
+		SceneManager.LoadScene (levelname);
+	}
+
+	IEnumerator LoadPrefabsCoroutine () {
+		yield return new WaitForEndOfFrame ();
+
+		foreach (GameObject gameobject in _prefabsToLoad) {
+			Instantiate (gameobject);
+		}
+	}
+}
